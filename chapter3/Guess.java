e7951cc842e9737588fee70fe972851300dc728f
imaxct <imaxct@foxmail.com>
2015-12-12 15:17:57 +0800

    Modified Some Files

--- chapter3/Guess.java
+++ chapter3/Guess.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 /**
  * @author Max_CT(imaxct)
  *
- * @since 2015年11月5日 下午1:33:53
+ * @since 2015-12-12 15:10
  * 
  *        Pro 3.2
  */
@@ -18,25 +18,33 @@ public class Guess {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner in = new Scanner(System.in);
-		int num = (int) (Math.random() * 1000) + 1;
-		int userinput = 0;
 
-		System.out.println("Input an integer between 1 and 1000:");
+		int turn = 0;
+		String command = "yes";
+		String confirm;
+		do {
+			turn++;
+			int num = (int) (Math.random() * 1000) + 1;
+			int userinput = 0;
+			System.out.println("============= Round " + turn + " ===============");
+			System.out.println("Input an integer between 1 and 1000:");
 
-		userinput = in.nextInt();
-
-		while (userinput != num) {
-			if (userinput > num)
-				System.out.println("It's too big, try again:");
-			else
-				System.out.println("It's too small, try again:");
 			userinput = in.nextInt();
-		}
 
-		System.out.println("You win!");
-
+			int guesed = 0;
+			while (userinput != num) {
+				guesed++;
+				if (userinput > num)
+					System.out.println("It's too big, try again:");
+				else
+					System.out.println("It's too small, try again:");
+				userinput = in.nextInt();
+			}
+			System.out.println("You win! Tried " + guesed + " times");
+			System.out.print("Play again?(yes/no)");
+			confirm = in.next();
+		} while (confirm.equalsIgnoreCase(command));
 		in.close();
-
 	}
 
 }
