6a76b6a664b864b75aed26172b24ec7a200ef2e6
imaxct <imaxct@foxmail.com>
2015-12-15 20:08:51 +0800

    modified someplace

--- chapter7/MergeSort.java
+++ chapter7/MergeSort.java
@@ -22,14 +22,14 @@ public class MergeSort {
 	public static void merge(int[] arr, int p, int q, int r) {
 		int n1 = q - p + 1;
 		int n2 = r - q;
-		int[] L = new int[arr.length];
-		int[] R = new int[arr.length];
+		int[] L = new int[r - p + 1];
+		int[] R = new int[r - p + 1];
 		for (int i = 0; i < n1; i++)
 			L[i] = arr[p + i];
 		for (int i = 0; i < n2; i++)
 			R[i] = arr[q + i + 1];
-		L[n1] = 0x3f3f3f3f;
-		R[n2] = 0x3f3f3f3f;
+		L[n1] = 0x7fffffff;
+		R[n2] = 0x7fffffff;
 		int i = 0, j = 0;
 		for (int k = p; k <= r; k++) {
 			if (L[i] <= R[j])
