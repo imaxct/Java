3652724e9df2b40bd0cb7d1bb6a0a67a24b7f23f
imaxct <imaxct@foxmail.com>
2015-12-21 20:46:11 +0800

    modified the TernarySearch

--- chapter7/TernarySearch.java
+++ chapter7/TernarySearch.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
  *
  * @since 2015年11月12日 下午9:30:27
  * 
- *        Pro 3.1 三分
+ *        Pro 3.1 三分 find the first element >= key
  */
 
 public class TernarySearch {
@@ -15,25 +15,23 @@ public class TernarySearch {
 		while (left < right) {
 			int mid = left + (left + right) / 3;
 			int mmid = right - (left + right) / 3;
-			if (key < arr[mid])
-				right = mid - 1;
-			else if (key > arr[mmid])
-				left = mmid + 1;
-			else if (key > arr[mid] && key <= arr[mmid]) {
-				left = mid + 1;
-				right = mmid;
-			} else if (key >= arr[mid] && key < arr[mmid]) {
+			System.out.println(mid + " " + mmid);
+			if (arr[mid] <= key && key <= arr[mmid]) {
 				left = mid;
-				right = mmid - 1;
+				right = mmid;
+			} else if (key < arr[mid]) {
+				right = mid - 1;
+			} else if (key > arr[mmid]) {
+				left = mmid + 1;
 			}
 		}
 		return left;
 	}
 
 	public static void main(String[] args) {
-		int[] array = { 0, 2, 3, 4, 5, 8, 9 };
+		int[] array = { 1, 2, 3, 3, 3, 6 };
 		Arrays.sort(array);
-		int pos = ternarySearch(array, 2, 0, array.length - 1);
+		int pos = ternarySearch(array, 4, 0, array.length - 1);
 		System.out.println(pos);
 	}
 }
